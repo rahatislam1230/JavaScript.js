5e0b4e7f9b13149ef8c491b6014c9458b2bcfcb6
Rahad Islam <rahatislam2100@gmail.com>
2025-02-12 14:55:49 +0600

    Create basic.js

---
+++ basic.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JavaScript Basics</title>
+</head>
+<body>
+    <script>
+        console.log("Hello, World!"); // Output to the browser console
+        alert("Hello, World!"); // Pop-up alert
+        document.write("Hello, World!"); // Write to the HTML document
+    </script>
+</body>
+</html>
